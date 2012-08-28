dc2b31d3ed8590511619ae07514291a2af5b703c
FUKUI Osamu <ofukui@irubysystems.com>
2012-08-28 15:29:58 +0900

    log git開始

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Log::Application.config.secret_token = '21873c124b272d08f92be6b36486e2ef96c8ecad5f46831a993aa585a386148500c361563ec5cb89ca2c172c4f6825ae72aeffe21bed9f2b6fa88ada7def9e85'
