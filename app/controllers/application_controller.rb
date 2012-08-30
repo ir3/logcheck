cf0dae11ec7ff24db0af2c420f29222c59a04069
IWAI, Masaharu <masaharu.iwai@ves.co.jp>
2012-08-30 12:48:59 +0900

    長いテキストを生成するメソッド「logn_text」を追加
    長いテキストを生成する「long_text」をアプリケーションコントローラーに追加。
    引数無しではASCII範囲のテキストを生成。
    引数に「ja」を与えるといわゆる全角文字を返す。

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,24 @@
+# coding: utf-8
+
 class ApplicationController < ActionController::Base
   protect_from_forgery
+
+  def long_text(lang="en")
+    text = ''
+
+    if lang == "ja" then
+      logger.info 'logtext: ja'
+      for i in (1..200)
+        text = text + sprintf("[%08d]",i).to_s
+      end
+      text.tr!('0-9[]','０-９すえ')
+    else
+      logger.info 'logtext: en'
+      for i in (1..500)
+        text = text + sprintf("[%08d]",i).to_s
+      end
+    end 
+
+    return text
+  end
 end
